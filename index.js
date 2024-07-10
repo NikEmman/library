3970169ac92cecdc49a90b7c059aaba877406cec
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-07-10 12:14:02 +0300

    Create form, add js to add items to library

---
+++ index.js
@@ -0,0 +1,44 @@
+const myLibrary = []
+const form = document.querySelector("form");
+
+function Book(title, author, pages, read, comments) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.comments = comments
+}
+function getFormValues() {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementsByName("read");
+    let comments = document.getElementById("comments").value;
+
+    let readValue;
+    for (let i = 0; i < read.length; i++) {
+        if (read[i].checked) {
+            readValue = read[i].value;
+            break;
+        }
+    }
+    return { title, author, pages, readValue, comments };
+
+}
+
+document.getElementById("submit").addEventListener("click", function (event) {
+    event.preventDefault();
+    addBookToLibrary();
+    form.reset();
+});
+
+
+function addBookToLibrary() {
+    const { title, author, pages, readValue, comments } = getFormValues();
+    myLibrary.push(new Book(title, author, pages, readValue, comments));
+};
+
+const addBtn = document.getElementById("addBook")
+addBtn.addEventListener("click", () => {
+    form.classList.toggle("hidden");
+})
