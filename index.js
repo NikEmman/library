cfae584e5d41c988eb0d3c9bf458315707f3dc0d
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-08-20 13:28:31 +0300

    Validation implemented

--- index.js
+++ index.js
@@ -26,10 +26,13 @@ function getFormValues() {
 }
 
 document.getElementById("submit").addEventListener("click", function (event) {
-  event.preventDefault();
-  addBookToLibrary();
-  form.reset();
-  appendBooks();
+  if (checkError()) {
+    event.preventDefault();
+  } else {
+    addBookToLibrary();
+    form.reset();
+    appendBooks();
+  }
 });
 
 function addBookToLibrary() {
@@ -88,3 +91,102 @@ cardContainer.addEventListener("click", (e) => {
   }
 });
 // form validations
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const comments = document.getElementById("comments");
+const pages = document.getElementById("pages");
+const titleError = document.getElementById("titleError");
+const authorError = document.getElementById("authorError");
+const pagesError = document.getElementById("pagesError");
+const commentsError = document.getElementById("commentsError");
+
+title.addEventListener("input", (e) => {
+  if (title.validity.valid) {
+    titleError.textContent = "";
+    titleError.className = "hidden";
+  } else {
+    showTitleError();
+  }
+});
+
+function showTitleError() {
+  if (title.validity.valueMissing) {
+    titleError.textContent = "You must to enter a title";
+    titleError.className = "error";
+  } else if (title.validity.tooShort) {
+    titleError.textContent = `Title must be at least ${title.minLength} characters long`;
+    titleError.className = "error";
+  }
+}
+author.addEventListener("input", (e) => {
+  if (author.validity.valid) {
+    authorError.textContent = "";
+    authorError.className = "hidden";
+  } else {
+    showAuthorError();
+  }
+});
+
+function showAuthorError() {
+  if (author.validity.valueMissing) {
+    authorError.textContent = "You must to enter a author";
+    authorError.className = "error";
+  } else if (author.validity.tooShort) {
+    authorError.textContent = `Author must be at least ${author.minLength} characters long`;
+    authorError.className = "error";
+  }
+}
+comments.addEventListener("input", (e) => {
+  if (comments.validity.valid) {
+    commentsError.textContent = "";
+    commentsError.className = "hidden";
+  } else {
+    showCommentsError();
+  }
+});
+
+function showCommentsError() {
+  if (comments.validity.valueMissing) {
+    commentsError.textContent = "You must to enter a comment";
+    commentsError.className = "error";
+  } else if (comments.validity.tooShort) {
+    commentsError.textContent = `Comments must be at least ${comments.minLength} characters long`;
+    commentsError.className = "error";
+  }
+}
+pages.addEventListener("input", (e) => {
+  if (pages.validity.valid) {
+    pagesError.textContent = "";
+    pagesError.className = "hidden";
+  } else {
+    showPagesError();
+  }
+});
+
+function showPagesError() {
+  if (pages.validity.valueMissing) {
+    pagesError.textContent = "Please enter a number";
+    pagesError.className = "error";
+  } else if (pages.validity.badInput) {
+    pagesError.textContent = "Please enter a number";
+    pagesError.className = "error";
+  } else if (pages.validity.rangeOverflow || pages.validity.rangeUnderflow) {
+    pagesError.textContent = `Enter an integer between ${pages.min} and ${pages.max}`;
+    pagesError.className = "error";
+  }
+}
+// fix me
+const fieldset = document.querySelector("fieldset");
+
+const yesRadio = document.querySelector('input[name="read"][value="Yes"]');
+const noRadio = document.querySelector('input[name="read"][value="No"]');
+const radioIsValid = yesRadio.checked || noRadio.checked;
+function checkError() {
+  return (
+    showPagesError() ||
+    showTitleError() ||
+    showAuthorError() ||
+    showCommentsError() ||
+    radioIsValid //fixme
+  );
+}
